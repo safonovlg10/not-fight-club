7c99c624d60a08c8987ff848a790b677cf9aedcb
safonovlg10 <safonovlg10@gmail.com>
2025-08-23 19:01:50 +0300

    feat: create gh-pages-test

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 import Register from "./views/register/Register.js";
 import Home from "./views/home/Home.js";
 import Setting from "./views/setting/Setting.js";
-import Battle from "./views/battle/battle.js";
+// import Battle from "./views/battle/battle.js";
 import Router from "./router/Router.js";
 // import charactersData from "./characters/charactersData.js";
 
@@ -11,7 +11,7 @@ const routes = {
   "/": () => new Home(),
   "/register": () => new Register(),
   "/setting": () => new Setting(),
-  "/battle": () => new Battle(),
+  // "/battle": () => new Battle(),
   //   '*': () => new NotFound()
 };
 
